15eaaae4c7326f539142c99fb5cbea6b5ac1fe21
ayush-more-11 <moreayush48@gmail.com>
2026-08-21 17:54:03 +0530

    Updated sccb with comments on blocks

--- edge_detection_v2.srcs/sources_1/new/sccb.v
+++ edge_detection_v2.srcs/sources_1/new/sccb.v
@@ -71,37 +71,46 @@ module ov5640_iic(
     wire                dir;  // direction - 0 for write and 1 for read 
 
 
-    always @(posedge sclk or negedge s_rst_n) begin
-        if(s_rst_n == 1'b0)
+    always @(posedge sclk or negedge s_rst_n) begin // Initialize i2c_clk
+        if(s_rst_n == 1'b0) // checks reset
             iic_clk <=  1'b1;
-        else if(start_latched == 1'b1) 
+        else 
+        if(start_latched == 1'b1) // checks latch
             iic_clk <= 1'b0;
-        else if(cfg_cnt == 'd28 && dir ==1'b1 && delay_cnt <='d3)
-            iic_clk     <= 1'b1;
-        else if(busy == 1'b1)
+        else 
+        if(cfg_cnt == 'd28 && dir ==1'b1 && delay_cnt <='d3) // when in read mode at 28th bit and dir = 0
+            iic_clk <= 1'b1;
+        else 
+        if(busy == 1'b1) // checks busy status
             iic_clk <= ~iic_clk;
-        else iic_clk <=  1'b1;
+        else 
+        iic_clk <=  1'b1;
     end
 
 
-    always @(negedge sclk or negedge s_rst_n) begin
+    always @(negedge sclk or negedge s_rst_n) begin // Initializes Busy
         if(s_rst_n == 1'b0)
             busy <= 1'b0;
-        else if(start_latched == 1'b1) 
+        else 
+        if(start_latched == 1'b1) // busy till latch completes the transaction
             busy <= 1'b1;
-        else if(done == 1'b1)
+        else 
+        if(done == 1'b1) 
             busy <= 1'b0;
     end
 
 
-    always @(negedge sclk or negedge s_rst_n) begin
+    always @(negedge sclk or negedge s_rst_n) begin // Initialize Counter
         if(s_rst_n == 1'b0)
             cfg_cnt <= 'd0;
-        else if((cfg_cnt >= 'd47 && dir == 1'b1) || (cfg_cnt >= 'd37 && dir == 1'b0))
+        else 
+        if((cfg_cnt >= 'd47 && dir == 1'b1) || (cfg_cnt >= 'd37 && dir == 1'b0)) // check the count for both write(37) and read(47)
             cfg_cnt <= 'd0;
-        else if(cfg_cnt == 'd28 && delay_cnt <= 'd4 && dir == 1'b1)
+        else 
+        if(cfg_cnt == 'd28 && delay_cnt <= 'd4 && dir == 1'b1) // pause at 28
             cfg_cnt <= 'd28;
-        else if(busy == 1'b1 && iic_clk ==1'b0)
+        else 
+        if(busy == 1'b1 && iic_clk ==1'b0) 
             cfg_cnt <= cfg_cnt + 1'b1;
     end
 
@@ -109,26 +118,30 @@ module ov5640_iic(
     always @(negedge sclk or negedge s_rst_n) begin
         if(s_rst_n == 1'b0)
             riic_data <= 'd0;
-        else if(iic_clk == 1'b1 && cfg_cnt >= 'd38 && flag_ack ==1'b1)
+        else 
+        if(iic_clk == 1'b1 && cfg_cnt >= 'd38 && flag_ack ==1'b1)
             riic_data   <= {riic_data[6:0],iic_sda};
     end
 
 
-    always @(negedge sclk or negedge s_rst_n) begin
+    always @(negedge sclk or negedge s_rst_n) begin // Initialize Done 
         if(s_rst_n == 1'b0)
             done <= 1'b0;
-        else if(dir == 1'b1 && cfg_cnt == 'd46 && iic_clk == 1'b1)
-            done    <=  1'b1;
-        else if(dir == 1'b0 && cfg_cnt == 'd36 && iic_clk == 1'b1)
-            done    <=  1'b1;
-        else done   <=  1'b0;
+        else 
+        if(dir == 1'b1 && cfg_cnt == 'd46 && iic_clk == 1'b1)
+            done <=  1'b1;
+        else 
+        if(dir == 1'b0 && cfg_cnt == 'd36 && iic_clk == 1'b1)
+            done <=  1'b1;
+        else done <=  1'b0;
     end
 
  
-    always @(posedge sclk or negedge s_rst_n) begin
+    always @(posedge sclk or negedge s_rst_n) begin // Initializing Delay when in read mode and at 28 bit
         if(s_rst_n == 1'b0)
             delay_cnt <= 'd0;   
-        else if(dir == 1'b1 && cfg_cnt == 'd28)
+        else 
+        if(dir == 1'b1 && cfg_cnt == 'd28)
             delay_cnt <= delay_cnt + 1'b1; 
         else 
             delay_cnt <= 'd0;
@@ -137,10 +150,11 @@ module ov5640_iic(
 // Module for ack at every 9th bit, - tri state ack
     always @(*) begin
         if(dir == 1'b1 && (cfg_cnt =='d9 || cfg_cnt =='d18 ||cfg_cnt =='d27 ||(cfg_cnt >='d37 &&cfg_cnt <='d45)))
-            flag_ack        <=1'b1; 
-        else if(dir == 1'b0 && (cfg_cnt =='d9 || cfg_cnt =='d18 ||cfg_cnt =='d27 ||cfg_cnt =='d36))
-            flag_ack        <=1'b1;  
-        else    flag_ack        <=1'b0;
+            flag_ack <=1'b1; 
+        else 
+        if(dir == 1'b0 && (cfg_cnt =='d9 || cfg_cnt =='d18 ||cfg_cnt =='d27 ||cfg_cnt =='d36))
+            flag_ack <=1'b1;  
+        else    flag_ack <=1'b0;
     end
         
 
@@ -149,8 +163,9 @@ module ov5640_iic(
             case(cfg_cnt)
                 0:
                     if(busy == 1'b1)
-                        iic_sda_reg = 1'b0;
-                    else    iic_sda_reg = 1'b1;
+                        iic_sda_reg = 1'b0; 
+                    else    
+                        iic_sda_reg = 1'b1;
                 //ID Address
                 1:      iic_sda_reg = w_data_reg[31];
                 2:      iic_sda_reg = w_data_reg[30];
@@ -184,9 +199,9 @@ module ov5640_iic(
                 //STOP & START
                 28:
                         if(delay_cnt <= 'd1 || delay_cnt >= 'd4)
-                            iic_sda_reg =       1'b0;
+                            iic_sda_reg = 1'b0;
                         else
-                            iic_sda_reg =       1'b1;
+                            iic_sda_reg = 1'b1;
                             
                 //ID        Address
                 29:     iic_sda_reg = w_data_reg[31];
@@ -215,7 +230,7 @@ module ov5640_iic(
                 5:      iic_sda_reg = w_data_reg[27];
                 6:      iic_sda_reg = w_data_reg[26];
                 7:      iic_sda_reg = w_data_reg[25];
-                8:      iic_sda_reg = 1'b0;  //写地址
+                8:      iic_sda_reg = 1'b0; 
                 
                 //Address High  Byte
                 10:     iic_sda_reg = w_data_reg[23];
@@ -248,7 +263,8 @@ module ov5640_iic(
                 35:     iic_sda_reg = w_data_reg[0];
                 
                 37:     iic_sda_reg = 1'b0;
-                default:iic_sda_reg = 1'b1;     
+                
+                default: iic_sda_reg = 1'b1;     
             endcase
     end
 
